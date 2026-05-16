83df336f3ca3048aacead5b5e91e21e09c21b361
shrecell <pathrikarshreyas@gmail.com>
2026-05-16 19:30:23 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,14 @@
+export let watch={
+    pname:"titan",
+    price:5000,
+    warranty:"1year",
+
+};
+ export let watch1={
+    pname:"sonata",
+    price:6000,
+    warranty:"1 and 1/2 year",
+ };
+ export default function test(){
+    console.log("this is to test the page");
+ }
